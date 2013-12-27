a5654ee89d15b766c666bc74a0fd89852e9b4bbe
Kyle Tolle <kyle@nullsix.com>
2013-12-27 00:38:34 -0500

    Add Map#to_s

--- lib/map.rb
+++ lib/map.rb
@@ -4,14 +4,20 @@ class Map
   def initialize(width, height)
     @width = width
     @height = height
+
+    generate
   end
 
   def generate
     @array ||= [].tap do |a|
       @height.times do
-        a << Row.new(@width).generate
+        a << Row.new(@width)
       end
     end
+  end
+
+  def to_s
+    @array.each(&:to_s).join("\n")
   end
 end
 
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Map do
   let(:width)  { 2 }
-  let(:height) { 4 }
+  let(:height) { 2 }
 
   subject{ Map.new(width, height) }
 
@@ -12,6 +12,13 @@ describe Map do
         select{|row| row.count == width }
 
       expect(rows_with_all_columns.count).to eq height
+    end
+  end
+
+  describe "to_s" do
+    it "has the right format" do
+      map_regex = /^\| .* | .* \|\n\| .* | .* \|$/
+      expect(subject.to_s).to match(map_regex)
     end
   end
 end
