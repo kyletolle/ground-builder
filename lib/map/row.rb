2a12f8cc90b67fad274dc4e01a0280415cf3ba93
Kyle Tolle <kyle@nullsix.com>
2013-12-27 00:38:06 -0500

    Add Row#to_s

--- lib/map/row.rb
+++ lib/map/row.rb
@@ -4,10 +4,16 @@ class Map
   class Row
     def initialize(width)
       @width = width
+
+      generate
     end
 
     def generate
-      @column ||= Column.new(@width).generate
+      @column ||= Column.new(@width)
+    end
+
+    def to_s
+      "| #{@column.to_s} |"
     end
   end
 end
--- spec/map/row_spec.rb
+++ spec/map/row_spec.rb
@@ -3,10 +3,18 @@ require 'spec_helper'
 describe Map::Row do
   let(:width) { 2 }
 
+  subject { Map::Row.new(width) }
+
   describe "#generate" do
     it "has all the columns" do
-      row = Map::Row.new(width).generate
-      expect(row.count).to eq width
+      expect(subject.count).to eq width
+    end
+  end
+
+  describe "#to_s" do
+    it "has the right format" do
+      map_regex = /^\| W:(\d\.\d\d);So:(\d\.\d\d);St:(\d\.\d\d) | W:(\d\.\d\d);So:(\d\.\d\d);St:(\d\.\d\d) \|$/
+      expect(subject.to_s).to match(map_regex)
     end
   end
 end
