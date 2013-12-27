4740a57fad14d5e84ddc758dd78c5a156c27e78f
Kyle Tolle <kyle@nullsix.com>
2013-12-26 21:28:48 -0500

    Remove driver code from ground_builder

--- lib/ground_builder.rb
+++ lib/ground_builder.rb
@@ -1,4 +1,3 @@
-require 'pp'
 require_relative 'ground'
 require_relative 'elements'
 require_relative 'map'
@@ -13,12 +12,4 @@ class GroundBuilder
     @map ||= Map.new(@width, @height)
   end
 end
-
-# puts "THE MAP"
-# puts "----------------"
-
-# width = Ground::Sizes::X
-# height = Ground::Sizes::Y
-
-# pp GroundBuilder.new(width, height).map
 
