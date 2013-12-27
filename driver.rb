83579cba05fb84fbd2e3383cfc52cb2c75b9e25c
Kyle Tolle <kyle@nullsix.com>
2013-12-27 00:39:33 -0500

    Driver prints formatted map

--- driver.rb
+++ driver.rb
@@ -2,10 +2,11 @@ require 'pp'
 require_relative 'lib/ground_builder'
 
 puts "THE MAP"
-puts "----------------"
+puts "="*26
+puts 
 
 width = Ground::Sizes::X
 height = Ground::Sizes::Y
 
-pp GroundBuilder.new(width, height).map
+puts GroundBuilder.new(width, height).map.to_s
 
