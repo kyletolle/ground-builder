6a917aa4d015d75da573c420ccc70392ccf719b7
Kyle Tolle <kyle@nullsix.com>
2013-12-26 23:18:37 -0500

    Refactor map spec

--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 
 describe Map do
-  describe "#generate" do
-    let(:width)  { 2 }
-    let(:height) { 4 }
+  let(:width)  { 2 }
+  let(:height) { 4 }
 
+  subject{ Map.new(width, height) }
+
+  describe "#generate" do
     it "has all the rows and columns given" do
-      map = Map.new(width, height).generate
-      expect(map.select{|row| row.count == width}.count).to eq height
+      rows_with_all_columns = subject.generate.
+        select{|row| row.count == width }
+
+      expect(rows_with_all_columns.count).to eq height
     end
   end
 end
