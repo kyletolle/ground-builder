4937d9865b3a75ea592f00fe0ac6a475bf93e2ba
Kyle Tolle <kyle@nullsix.com>
2014-05-02 20:29:10 -0400

    Make GroundBuilder use Lifecycle, et al

--- lib/ground_builder.rb
+++ lib/ground_builder.rb
@@ -1,8 +1,12 @@
 require_relative 'ground'
-require_relative 'elements'
 require_relative 'map'
+require_relative 'lifecycle'
+require_relative 'dropper'
+require_relative 'settler'
 
 class GroundBuilder
+  ITERATIONS = 5
+
   def initialize(width, height)
     @width = width
     @height = height
@@ -10,6 +14,13 @@ class GroundBuilder
 
   def map
     @map ||= Map.new(@width, @height)
+  end
+
+  def build
+    dropper_class = Dropper
+    settler_class = Settler
+    Lifecycle.new(map, dropper_class, settler_class, ITERATIONS).live
+    self
   end
 end
 
