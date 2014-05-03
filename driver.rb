fd20742f8cce866157b38977b18353389d86bc28
Kyle Tolle <kyle@nullsix.com>
2014-05-02 20:29:59 -0400

    Update driver for GroundBuilder changes

--- driver.rb
+++ driver.rb
@@ -3,10 +3,11 @@ require_relative 'lib/ground_builder'
 
 puts "THE MAP"
 puts "="*26
-puts 
+puts
 
 width = Ground::Sizes::X
 height = Ground::Sizes::Y
 
-puts GroundBuilder.new(width, height).map.to_s
+ground_builder = GroundBuilder.new(width, height).build
+puts ground_builder.map.to_s
 
