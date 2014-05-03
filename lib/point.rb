41cb405bd22cb1faf9db5426ab1f1dae1467827a
Kyle Tolle <kyle@nullsix.com>
2014-05-02 20:11:03 -0400

    Add bits to Point; can drop a bit on a point

--- lib/point.rb
+++ lib/point.rb
@@ -1,17 +1,21 @@
 require_relative 'affinity'
 
 class Point
-  attr_reader :affinity
+  attr_reader :affinity, :bits
 
   def initialize
     random_index = rand(0...Elements.all.count)
     @affinity = Affinity.new(Elements.all[random_index], 1.0)
+    @bits = []
   end
 
   def to_s
     formatted_weight = '%.2f' % affinity.weight
     "#{affinity.element.abbr}:#{formatted_weight}".rjust(7, " ")
   end
+
+  def drop(bit)
+    @bits << bit
   end
 end
 
